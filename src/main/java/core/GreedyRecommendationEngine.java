75f94225f0f2182fc650f2c61b4b21522e162b78
yuyuuxP <nayu82004@gmail.com>
2026-06-15 22:01:26 +0800

    fix: memperbaiki algoritma greedy

--- src/main/java/core/GreedyRecommendationEngine.java
+++ src/main/java/core/GreedyRecommendationEngine.java
@@ -7,113 +7,41 @@ import java.util.List;
 
 public class GreedyRecommendationEngine {
 
-    //tahap 1 disini filter produk brdsrkn skin type si cust
-    public static List<Product> filterBySkinType(
-            List<Product> products,
-            String skinType) {
-
+    // produk akan difilter berdasarkan jenis kulit customer/pembeli
+    public static List<Product> filterBySkinType(List<Product> products, String skinType) {
         List<Product> filtered = new ArrayList<>();
-
         for (Product product : products) {
-
-            if (product.getSkinType()
-                    .equalsIgnoreCase(skinType)) {
-
+            if (product.getSkinType().equalsIgnoreCase(skinType) || product.getSkinType().equalsIgnoreCase("Semua Kulit")) {
                 filtered.add(product);
             }
         }
-
         return filtered;
     }
 
-    //tahap 2 disini sort brdsrkn rating tertinggi
-    public static List<Product> sortByHighestRating(
-            List<Product> products) {
-
+    // proses mengurutkan produk sesuai rating tertinggi
+    public static List<Product> sortByHighestRating(List<Product> products) {
         List<Product> sorted = new ArrayList<>(products);
-
-        sorted.sort(
-                Comparator.comparingDouble(Product::getRating)
-                          .reversed()
-        );
-
+        sorted.sort(Comparator.comparingDouble(Product::getRating).reversed());
         return sorted;
     }
 
-    //tahap 3 disni mi bekerja ki greedy selection nya
-    public static GreedyResult recommendProducts(
-            List<Product> products,
-            int budget) {
-
-        List<Product> sortedProducts =
-                sortByHighestRating(products);
-
-        List<Product> selectedProducts =
-                new ArrayList<>();
+    // Di sini algoristma greedy sudah jalan (sistem ambil 2 produk yg premium)
+    public static GreedyResult recommendProducts(List<Product> products, int budget) {
+        List<Product> sortedProducts = sortByHighestRating(products);
+        List<Product> selectedProducts = new ArrayList<>();
 
         int remainingBudget = budget;
         int totalPrice = 0;
         double totalRating = 0;
-
+        int maxPremiumItems = 2;
         for (Product product : sortedProducts) {
-
-            if (product.getPrice() <= remainingBudget) {
-
+            if (product.getPrice() <= remainingBudget && selectedProducts.size() < maxPremiumItems) {
                 selectedProducts.add(product);
-
                 remainingBudget -= product.getPrice();
-
                 totalPrice += product.getPrice();
-
                 totalRating += product.getRating();
             }
         }
-
-        return new GreedyResult(
-                selectedProducts,
-                totalPrice,
-                totalRating,
-                remainingBudget
-        );
-    }
-
-    //buat kasi tmpil hsil nya
-    public static void printResult(
-            GreedyResult result) {
-
-        System.out.println();
-        System.out.println("=================================");
-        System.out.println("     GREEDY RECOMMENDATION");
-        System.out.println("=================================");
-
-        for (Product p :
-                result.getSelectedProducts()) {
-
-            System.out.printf(
-                    "- %s | Rp%,d | Rating %.1f%n",
-                    p.getProductName(),
-                    p.getPrice(),
-                    p.getRating()
-            );
-        }
-
-        System.out.println("---------------------------------");
-
-        System.out.printf(
-                "Total Harga     : Rp%,d%n",
-                result.getTotalPrice()
-        );
-
-        System.out.printf(
-                "Total Rating    : %.1f%n",
-                result.getTotalRating()
-        );
-
-        System.out.printf(
-                "Sisa Budget     : Rp%,d%n",
-                result.getRemainingBudget()
-        );
-
-        System.out.println("=================================");
+        return new GreedyResult(selectedProducts, totalPrice, totalRating, remainingBudget);
     }
 }
